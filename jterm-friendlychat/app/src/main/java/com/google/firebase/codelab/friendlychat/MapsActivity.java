6e8e3b6ac0f2c9a0f8ea5638f927f0e9f7936be3
Demo User <demouser@MacBook-Air.local>
2017-01-20 14:56:19 -0500

    updates to chitter

--- jterm-friendlychat/app/src/main/java/com/google/firebase/codelab/friendlychat/MapsActivity.java
+++ jterm-friendlychat/app/src/main/java/com/google/firebase/codelab/friendlychat/MapsActivity.java
@@ -47,11 +47,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        String loc = '35.731252, 139.730291' // Tokyo expressed as lat,lng tuple
-         targetDate = new Date(); // Current date/time of user computer
-        var timestamp = targetDate.getTime()/1000 + targetDate.getTimezoneOffset() * 60 // Current UTC date/time expressed as seconds since midnight, January 1, 1970 UTC
-        String apikey = 'YOUR_TIMEZONE_API_KEY_HERE'
-        var apicall = 'https://maps.googleapis.com/maps/api/timezone/json?location=' + loc + '&timestamp=' + timestamp + '&key=' + apikey
 
     }
 }
